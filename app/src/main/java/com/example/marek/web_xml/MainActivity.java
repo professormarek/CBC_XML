41d0115ea6cb1c431439c835b7136afc002696fd
marek.laskowski <marek.laskowski@senecacollege.ca>
2016-10-20 16:18:48 -0400

    added stuff

--- app/src/main/java/com/example/marek/web_xml/MainActivity.java
+++ app/src/main/java/com/example/marek/web_xml/MainActivity.java
@@ -1,24 +1,90 @@
 package com.example.marek.web_xml;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
+
+    //use me to store headlines
+    Vector<String> headlines;
+    //similarly for urls.. etc.
+
+    /**
+     * this will be called from your other code when you want to perform the
+     * task in the background
+     */
+    private class DownloadAndParseXML extends AsyncTask<URL, Integer, Long >{
+        protected Long doInBackground(URL... urls){
+            int count = urls.length;
+            long num = 0;
+            for(int i = 0; i < count; i++){
+                //call our downloadAndParse function
+                num += downloadAndParse(urls[i]);
+                //handle cancelling the download
+                if(isCancelled()) break;
+            }
+            return num;
+        }
+
+        /**
+         * this gets called by the framework to update your activity on the
+         * task progress
+         * @param progress
+         */
+        protected void onProgressUpdate(Integer... progress){
+            System.out.println("progress: " + progress[0]);
+        }
+
+        /**
+         * onPostExecute gets called once your background task finishes
+         * here you want to update your UI when the task is done
+         * ex. update the headlines list in the lab 6
+         */
+         protected void onPostExecute(Long result){
+             System.out.println("AsyncTask Complete");
+         }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //open a URL connection and parse the xml that is returned
+        URL url = null;
+        try{
+            url = new URL("http://www.cbc.ca/cmlink/rss-topstories");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //actually start the download by instantiating our AsyncTask and calling execute
+        new DownloadAndParseXML().execute(url);
+
+    }
+
+    /**
+     * this function is called from our AsyncTask in order to actually download and parse
+     * the XML
+     * @param url
+     * @return
+     */
+    public long downloadAndParse(URL url){
         //let's parse some xml
-        //for now let's hard-code a string of xml
-        String myXML = "<links> <twitter>http://twitter.com</twitter> <fb>http://facebook.com</fb></links>";
         try {
+            //open a network socket
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(urlConnection.getInputStream());
+
             //get an instance of the XmlPullParserFactory (uses a factory pattern itself!)
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             //set the factory to be namespace aware
@@ -29,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
             //tell the parser where to get data from
             //note we can pass any Reader class to the parser
             //in this case, just do something simple with the string above
-            xpp.setInput(new StringReader(myXML));
+            xpp.setInput(in);
             //events are how the XmlPullParser communicates information to your method
             int event = xpp.getEventType();
             while(event != XmlPullParser.END_DOCUMENT){
@@ -51,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
         }catch(Exception e){
             e.printStackTrace();
         }
-
+        return 1;
     }
+
 }
